b00e06fa2539dabf987150140225fceb3fed5459
ZAKARYA123J <118215217+ZAKARYA123J@users.noreply.github.com>
2023-12-08 04:22:18 +0100

    ys

--- src/Productinfo.js
+++ src/Productinfo.js
@@ -75,7 +75,11 @@ export default function Productinfo(props) {
                 <span className="text-2xl leading-none align-baseline"></span>
               </div>
               <div className="inline-block align-bottom">
-                <button style={{paddingLeft:'10px'}}className="bg-yellow-300 opacity-75 hover:opacity-100 text-yellow-900 hover:text-gray-900 rounded-full px-10 py-2 font-semibold"><i className="mdi mdi-cart -ml-2 mr-2"></i><Link to={'/ReservationForm'}> BUY NOW</Link></button>
+              <button style={{ paddingLeft: '10px' }} className="bg-yellow-300 opacity-75 hover:opacity-100 text-yellow-900 hover:text-gray-900 rounded-full px-10 py-2 font-semibold">
+  <i className="mdi mdi-cart -ml-2 mr-2"></i>
+  <Link to={'/ReservationForm'}> BUY NOW</Link>
+</button>
+
               </div>
             </div>
           </div>
